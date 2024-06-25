6e4b96b2213bcdd0549919fd4c83c44c9765aa6d
Thomas Fritsche <[t.fritsche@reply.de]>
2024-06-25 15:10:52 +0200

    implement binary tree breath first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,19 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
 
+    while (q.length) {
+        const current = q.shift() as BinaryNode<number> | null | undefined;
+        if (!current) {
+            continue;
+        }
+        // search
+        if (current.value === needle) {
+            return true;
+        }
+        // @ts-ignore
+        q.push(current.left);
+        // @ts-ignore
+        q.push(current.right);
+    }
+    return false;
 }
